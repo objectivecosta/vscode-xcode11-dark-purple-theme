b7a8e3f9bf1259421f7eaa3ce9ba4b90067cf828
Rafael Costa <rafael@rafaelcosta.me>
2019-09-09 11:57:35 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-mkdir ~/.vscode/extensions/xcode-11-dark-purple-theme
+rm -rf ~/.vscode/extensions/xcode-11-dark-purple-theme
+mkdir -p ~/.vscode/extensions/xcode-11-dark-purple-theme
 cp -R ./* ~/.vscode/extensions/xcode-11-dark-purple-theme
